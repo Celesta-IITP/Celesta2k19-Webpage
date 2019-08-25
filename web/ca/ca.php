8da80bf2fdca47135f877fbd120a7d97887cd420
Ashwani Yadav <ashyadavash@gmail.com>
2019-08-25 13:12:02 +0530

    added ca

--- web/ca/ca.php
+++ web/ca/ca.php
@@ -7,183 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
     <link rel="stylesheet" href="./css/main.css" />
+    <link rel="stylesheet" href="./css/ca-styles.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
-    <style>
-        .imgClass {
-            position: absolute;
-
-        }
-
-        .imgClass img {
-            max-width: 100%;
-            height: auto;
-        }
-
-
-        .fb2css {
-            padding: 10px;
-            background-color: rgba(98, 153, 193, 0.76);
-            margin: 30px;
-            border-radius: 10px;
-            left: 50%;
-            /*position: absolute;*/
-            /*transform: translateX(-50%);*/
-            width: 85% !important;
-            width: 300px;
-        }
-
-        #FB-Oauth {
-            padding: 10px;
-            background-color: rgba(98, 153, 193, 0.76);
-            margin: 30px;
-            border-radius: 10px;
-            left: 50%;
-            /*position: absolute;*/
-            /*transform: translateX(-50%);*/
-            /* width: 90% !important; */
-            width: 300px;
-        }
-
-        /*@media screen and (max-width: 600px) {
-			#FB-Oauth,#FB-Oauth2{
-				width: 300px;
-			}} */
-        @media screen and (max-width:4000px) {
-            .img6 {
-                top: 0px;
-                right: 0px;
-                width: 490px;
-            }
-
-            .img5 {
-                top: 0px;
-                left: 0px;
-                width: 400px;
-            }
-        }
-
-        @media screen and (max-width:1100px) {
-            .img5 {
-                top: 0px;
-                left: 0px;
-                width: 300px;
-            }
-
-            .img6 {
-                top: 0px;
-                right: 0px;
-                width: 400px;
-            }
-        }
-
-        @media screen and (max-width:980px) {
-            section blockquote {
-                font-size: 1.2em;
-            }
-
-        }
-
-        @media screen and (max-width:900px) {
-            .img6 {
-                top: 0px;
-                right: 0px;
-                width: 320px;
-            }
-
-            .img5 {
-                top: 0px;
-                left: 0px;
-                width: 300px;
-            }
-        }
-
-        @media screen and (max-width:736px) {
-
-            .img1 {
-                height: 180px;
-                bottom: 40px;
-                left: 40px;
-                width: 150px;
-            }
-
-            .img2 {
-                top: 0px;
-                height: 200px;
-                width: 100px;
-                right: 0px;
-            }
-
-            .img3 {
-                top: 0px;
-                left: 0px;
-                height: 100px;
-                width: 100px;
-
-
-            }
-
-            .img4 {
-                top: 5%;
-                left: 0px;
-                width: 300px;
-                margin: 0px auto;
-
-            }
-
-            .imgcontainer {
-                top: 0px;
-                left: 0;
-                width: 100%;
-                right: 0;
-            }
-
-            #header {
-                height: 100%;
-                width: 100%;
-
-            }
-
-            #header center {
-                position: absolute;
-                width: 100%;
-                top: 61%;
-                z-index: 20;
-                left: -11px;
-                margin-top: -100px;
-            }
-
-            .feature .content :last-child {
-                text-align: justify;
-            }
-
-            section blockquote {
-                font-size: 1.1em;
-            }
-
-            .type2 {
-                display: none;
-            }
-        }
-
-
-        @media screen and (min-width:737px) {
-
-            .type1 {
-                display: none;
-            }
-
-        }
-    </style>
 </head>
 
 <body>
 
     <!-- Header -->
     <div id="header">
-        <!-- style=" background:url(../assets/img/ca_bg.jpeg);background-repeat: no-repeat;background-size: 100% 100%;" -->
-        <!-- <span class="logo icon "></span>--><br><br><br><br>
+        <br><br><br><br>
         <h6 style="color:red">
             <!-- IIT Patna's -->
         </h6>
@@ -212,10 +46,6 @@
             <h1 style="font-size: 40px">About us</h1><br>
             <p>
                 Celesta is the annual techno-management extravaganza celebrated with utmost pomp and vigour, in autumn. Since it's inception in 2010, it has grown into one of the most anticipated student organized youth festival. From NASA scientists to stupendous dancers, from ethical hackers to spellbinding singers, from game development to gaming war, from robotics to dramatics, from Model United Nations to foreign exchange conferences, from Sufi to Death Metal, Celesta, lives up to it's motto "Think. Dream. Live.", urging every individual to unleash their inner passion to reach their ultimate dream. Student fraternity who work day in day out, pouring out all of their heart into promoting intellectualism and creativity, while also maintaing the brand value of one of the most prestigious institute of the nation, IIT. Celesta, derived from the Sanskrit word meaning "quest" aims at unbridling the soul of every individual into the quest of finding one's self and reaching to his/her full potential. The latest edition of Celesta saw a footfall of around 15 K, of people belonging to a plethora of sectos such as computer geeks, gaming freaks, technoholics, music maniacs, dance enthusiasts and rock bands hailing from well-known institutes across the country. </p>
-            <!--
-					<p>Tellus erat mauris ipsum fermentum<br />
-					etiam vivamus nunc nibh morbi.</p>
-					-->
         </header>
 
         <div class="box alt container">
@@ -249,62 +79,7 @@
                 <blockquote> Hello IITP, I remember I went Anwesha when I was in the first year of my college .I was scared but yeah that experience pulled me again and again and Anwesha 2K18 was my third year in a row.And this year was Damn amazing..The theme was magical ..The environment was spellbinding. And Yeah The thing you guys implemented this year this College ambassador thing was really great idea.. I really enjoyed working and sharing the notifications of Anwesha 2k18 In between of those times sometimes I started feeling like I am the part of the Team and yeah It was great being a part of such great environment and such enthusiastic People.. Anwesha is not just a college fest it feels like it's a Festival that comes every year and we all celebrate it together.. 🖤 <br>-<b>Mridul Choudhary</b> </blockquote>
                 <blockquote>As you mentioned Anwesha the biggest techno-cultural fest of north-east India being a campus ambassador for it was an amazing experience.You'll organised the whole function perfectly.I not only enjoyed being working as the campus ambassador but also enjoyed your fest.The participants from my college were also very happy by your organisation.Hope to continue to work with you all more as i gained a good experience. Thank you for selecting me as the campus ambassador of my college. <br>-<b>Kamal Kant</b> </blockquote>
             </section>
-            <!--<header>
-						<h2>Judging criteria:-</h2>
-					</header>
-					<section>
-   
-   <h3>Points Criteria: </h3>
-   <ul class="default">
-   	<li>Online registrations from your referral id - 80 snickles per registration</li>
-   	<li>Upgrade profle photo / cover photo on facebook with Anwesha frame #Anwesha_2k18 in caption - 100 galleons per update</li>
-   	<li>Sharing posts on facebook with #Anwesha_2k19 in caption - 60 snuckles per share</li>
-   	<li>Offline registration from your referral id - 120 galleons per registration</li>
-   	<li>Mailer's Forward Screenshot - 60pts per mail</li>
-   	<li>Sharing Anwesha posts in Instagram Screenshot - 60pts per share</li>
-   	<li>Whatsapp group share Screenshot - 60pts per share</li>
-   	<li>Profile photo with Anwesha frame on instagram Screenshot - 100pts per update</li>
-   	<li>Profile photo with Anwesha frame on whatsapp Screenshot - 100pts per update</li>
-   	<li>Sharing contacts of Student body of your college - 10pts per contact</li>
-   	<li>Sharing details of Anwesha in your college's facebook page (if any) and tagging Anwesha (not in a guest post) - 100pts per share</li>
-   	<li>Sharing details of Anwesha in your college's whatsapp group (if any) Screenshot - 100pts per share</li>
-   </ul>
-<h3>Star CAs:</h3>   
-     After anwesha is over, the top 10 campus ambassadors of Anwesha will be the 'Star' CAs who will be given various perks along with cerificate which include goodies like brand new earphones
-     , anwesha t-shirts, coupons, etc. 
-   
-   <h3>Gold CAs:</h3>
-     Other top 40 CAs will receive Certificate of Campus Ambassador authorised by Anwesha, IIT Patna. 
-					-->
-            <!-- <section>
-						<header><br>
-							<h2>Responsibilities of CA</h2>
-						</header>
-						<ul class="default">
 
-						<li><b>Registration:</b><br>
-						   When the registration for Anwesha starts, register yourself as CA and then make other participants from your college register through your referral ID.</li>
-
-						   <li><b>Social Media:</b><br>
-						   Share posts, events and updates related info in social media platforms like facebook, twitter, Instagram.</li>
-
-						   <li><b>Participation:</b><br>
-						   Ensure keen participation from your college in Anwesha and multi-city auditions.</li>
-						</p>
-							
-						</ul>
-					</section> -->
-            <!-- <section>
-						<header>
-							<h2>Benefits of CA::</h2>
-						</header>
-						<p>
-					   Get a certificate of campus ambassador program from Anwesha IIT Patna.<br>
-					   Get a chance to click a selfie with the celebrities coming at pro nites.<br>
-					   Recieve goodies like anwesha t-shirts, earphones(boat), (other stuff from our sponsors)..</p>
-						<hr />
-						
-					</section> -->
             <section id="leader">
                 <header>
                     <h2>Leaderboard</h2>
@@ -326,16 +101,6 @@
             </section>
 
         </div>
-
-        <!-- 
-				<footer class="major container 75%">
-					<h3>Get shady with science</h3>
-					<p>Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus.</p>
-					<ul class="actions">
-						<li><a href="#" class="button">Join our crew</a></li>
-					</ul>
-				</footer> -->
-
     </div>
 
     <!-- Footer -->
@@ -348,9 +113,6 @@
                     <h2>Step 1</h2>
                     <div id="FB-Oauth2" class="fb2css">
                         <div class="fb-text">Sign-Up</div>
-                        <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-auto-logout-link="true" data-use-continue-as="true" data-scope="email,public_profile,user_location,user_likes,user_posts,user_birthday,user_about_me" onlogin="auth_response_change_callback();"></div>
-                        <!-- <br>
-							<a id="test">This this</a> -->
                     </div>
                 </center>
             </header>
@@ -416,12 +178,6 @@
                         <textarea name="involvement" placeholder="Have you been a part of one or more previous editions of Anwesha? If yes, please explain." required rows="4"></textarea>
                     </div>
                 </div>
-
-                <!-- <div class="row">
-							<div class="12u">
-								<input type="text" name="referalcode" placeholder="Optional referal code" />
-							</div>
-						</div> -->
                 <div id="message2"></div>
                 <div class="row">
                     <div class="12u">
@@ -446,7 +202,6 @@
 
         </div>
     </div>
-
 </body>
 
 </html>
