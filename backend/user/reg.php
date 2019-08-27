0a668450f925acd31ac6dc3aa8ad0a01c2fe36b0
Amartya Mondal <hayyoulistentome@gmail.com>
2019-08-27 23:25:20 +0530

    feat: Signup page

--- backend/user/reg.php
+++ backend/user/reg.php
@@ -1,3 +1,4 @@
+<?php include("functions/init.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Celesta2k19</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./css/reg_styles.css">
 </head>
 
 <body>
@@ -17,84 +18,112 @@
     <section class="login-body">
         <div class="row">
             <div class="input-cart col s12 m10 push-m1 z-depth-2">
+            <?php display_message() ?>
+            <?php login_signup() ?>
                 <div class="col s12 m7 signup">
+                
                     <div class="signupForm">
                         <h4 class="center">Sign up</h4>
                         <br>
-                        <form action="regCheck.php" name="signup" method="post" autocomplete="off">
+                        <form id="signup_form" name="signup_form" method="post" autocomplete="off">
+                            <!-- Name -->
                             <div class="row">
                                 <div class="input-field col s12 m6">
-                                    <input type="text" id="name-picked" name="namepicked" class="validate"
-                                        required="required" placeholder="Enter a username">
+                                    <input type="text" id="first_name" name="first_name" class="validate"
+                                        required="required" placeholder="First Name">
                                     <label for="name-picked">
                                         <i class="material-icons">person_add</i>
                                     </label>
                                 </div>
                                 <div class="input-field col s12 m6">
-                                    <input type="password" id="pass-picked" name="passpicked" class="validate"
+                                    <input type="text" id="last_name" name="last_name" class="validate"
+                                        placeholder="Last Name">
+                                    <label for="name-picked">
+                                        <i class="material-icons">person_add</i>
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- Name -->
+                            <div class="row">
+                                <div class="input-field col s12 m6">
+                                    <input type="text" id="phone" name="phone" class="validate"
+                                        required="required" placeholder="Phone Number">
+                                    <label for="phone-picked">
+                                        <i class="material-icons">phone</i>
+                                    </label>
+                                </div>
+                                <div class="input-field col s12 m6">
+                                    <input type="text" id="college" name="college" class="validate"
+                                        required="required" placeholder="School/College">
+                                    <label for="phone-picked">
+                                        <i class="material-icons">school</i>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field email">
+                                    <div class="col s12">
+                                        <input type="text" id="email" name="email" class="validate" required="required"
+                                            placeholder="Enter your email">
+                                        <label for="email">
+                                            <i class="material-icons">mail</i>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s12 m6">
+                                    <input type="password" id="password" name="password" class="validate"
                                         required="required" placeholder="Password">
+                                    <label for="pass-picked">
+                                        <i class="material-icons">lock</i> </label>
+                                </div>
+                                <div class="input-field col s12 m6">
+                                    <input type="password" id="confirm_password" name="confirm_password" class="validate"
+                                        required="required" placeholder="Confirm Password">
                                     <label for="pass-picked">
                                         <i class="material-icons">lock</i> </label>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="input-field email">
-                                    <div class="col s12">
-                                        <input type="text" id="email" name="email" class="validate" required="required"
-                                            placeholder="Enter your email">
-                                        <label for="email">
-                                            <i class="material-icons">mail</i>
-                                        </label>
-                                    </div>
+                                <div class="switch col s6">
+                                    <label>
+                                        <input type="checkbox" name="gender" id="gender" value="m">
+                                        <span class="lever"></span>
+                                        Male
+                                    </label>
+                                    <label>
+                                        <input type="checkbox" name="gender" id="gender" value="f">
+                                        <span class="lever"></span>
+                                        Female
+                                    </label>
                                 </div>
                             </div>
+                            <input type="hidden" id="signup" name="signup" value="love_u_atreyee" >
                             <div class="row">
-                                <div class="input-field email">
-                                    <div class="col s12">
-                                        <input type="text" id="email" name="email" class="validate" required="required"
-                                            placeholder="Enter your email">
-                                        <label for="email">
-                                            <i class="material-icons">mail</i>
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="input-field email">
-                                    <div class="col s12">
-                                        <input type="text" id="email" name="email" class="validate" required="required"
-                                            placeholder="Enter your email">
-                                        <label for="email">
-                                            <i class="material-icons">mail</i>
-                                        </label>
-                                    </div>
-                                </div>
+                                <button type="submit" name="btn-signup" class="btn blue right waves-effect waves-light">Sign Up</button>
                             </div>
                         </form>
-                        <div class="row">
-                            <button type="submit" name="btn-signup" class="btn blue right waves-effect waves-light">Sign Up</button>
-                        </div>
                     </div>
                     <div class="signup-toggle center">
                         <h4 class="center">Have No Account ? <a href="#!">Sign Up</a></h4>
                     </div>
                 </div>
-
                 <div class="col s12 m5 login">
                     <h4 class="center">Log in</h4>
                     <br>
-                    <form action="check.php" method="post" autocomplete="off">
+                    <form id="login_form" name="login_form" method="post" autocomplete="off">
                         <div class="row">
                             <div class="input-field">
-                                <input type="text" id="user" name="username" class="validate" required="required"
-                                    placeholder="Username">
+                                <input type="text" id="celestaid" name="celestaid" class="validate" required="required"
+                                    placeholder="Celesta ID">
                                 <label for="user">
                                     <i class="material-icons">person</i> </label>
                             </div>
                         </div>
                         <div class="row">
                             <div class="input-field">
-                                <input type="password" id="pass" name="password" class="validate" required="required"
+                                <input type="password" id="password" name="password" class="validate" required="required"
                                     placeholder="Password">
                                 <label for="pass">
                                     <i class="material-icons">lock</i>
@@ -104,13 +133,13 @@
                         <div class="row">
                             <div class="switch col s6">
                                 <label>
-                                    <input type="checkbox">
+                                    <input type="checkbox" name="remember" id="password">
                                     <span class="lever"></span>
                                     Remember Me
                                 </label>
                             </div>
                             <div class="col s6">
-                                <button type="submit" name="login" class="btn waves-effect waves-light blue right">Log
+                                <button type="submit" name="login" id="login" class="btn waves-effect waves-light blue right">Log
                                     in</button>
                             </div>
                         </div>
@@ -141,19 +170,19 @@
             <a class="btn-floating btn-large amber black-text">
                 Login
             </a>
-            <ul>
+            <!-- <ul>
                 <li><a class="indigo center" href="#!">Login with FB</a></li>
                 <li><a class="blue center" href="#!">Login with Twitter</a></li>
                 <li><a class="red center" href="#!">Login with Google +</a></li>
-            </ul>
+            </ul> -->
         </div>
     </section>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-    <script src="main.js"></script>
+    <script src="./js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/2.1.0/gl-matrix.js"></script>
-    <script src="canvas.js"></script>
+    <script src="./canvas/canvas.js"></script>
 </body>
 
 </html>
