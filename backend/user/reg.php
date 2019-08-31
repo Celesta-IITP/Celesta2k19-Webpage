ddbab2a4f279b840d590bd7b23f32bb3a48c9062
Ashwani Yadav <ashyadavash@gmail.com>
2019-08-31 13:35:06 +0530

    added events from json file

--- backend/user/reg.php
+++ backend/user/reg.php
@@ -187,6 +187,8 @@
     <script src="./js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/2.1.0/gl-matrix.js"></script>
     <script src="./canvas/canvas.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 
 </html>
