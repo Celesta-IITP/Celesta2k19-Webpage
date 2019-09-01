e853445d45b5892601662bbe07b5b1cbf52ea07f
Ashwani Yadav <ashyadavash@gmail.com>
2019-09-01 19:04:33 +0530

    revamped ca reg

--- backend/user/ca_register.php
+++ backend/user/ca_register.php
@@ -1,52 +1,54 @@
 <?php include("functions/init.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Campus Ambassador Registration</title>
-    <!-- Main css -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/ca_reg.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Campus Ambassador Registration</title>
+	<!-- Main css -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="./css/ca_reg.css">
 	<script src="./js/ca_reg.js"></script>
 </head>
+
 <body>
 
-    <div class="wrapper">
-	<div class="container">
-        <h1>Register for Campus Ambassador</h1>
-        <?php validate_ca_registration() ?> 
-		<form class="form" method="POST" id="signup-form">
-            <input type="text" class="form-input" name="first_name" id="first_name" placeholder="First Name"/>
-            <input type="text" class="form-input" name="last_name" id="last_name" placeholder="Last Name"/>
-            <input type="text" class="form-input" name="phone" id="phone" placeholder="Phone"/>
-            <input type="text" class="form-input" name="college" id="college" placeholder="School/College"/>
-            <input type="email" class="form-input" name="email" id="email" placeholder="Email"/>
-            <input type="password" class="form-input" name="password" id="password" placeholder="Password"/>
-            <input type="password" class="form-input" name="confirm_password" id="confirm_password" placeholder="Confirm Password"/>
+	<div class="wrapper">
+		<div class="container">
+			<h1>Register for Campus Ambassador</h1>
+			<?php validate_ca_registration() ?>
+			<form class="form" method="POST" id="signup-form">
+				<input type="text" class="form-input" name="first_name" id="first_name" placeholder="First Name" />
+				<input type="text" class="form-input" name="last_name" id="last_name" placeholder="Last Name" />
+				<input type="text" class="form-input" name="phone" id="phone" placeholder="Phone" />
+				<input type="text" class="form-input" name="college" id="college" placeholder="School/College" />
+				<input type="email" class="form-input" name="email" id="email" placeholder="Email" />
+				<input type="password" class="form-input" name="password" id="password" placeholder="Password" />
+				<input type="password" class="form-input" name="confirm_password" id="confirm_password" placeholder="Confirm Password" />
 
-	                    <input type="radio" name="gender" value="m" checked placeolder="Male">
-                            <span style="color:white"> Male </span>
-	                    <input type="radio" name="gender" value="f" >
-	                        <span style="color:white">Female </span><br>
-			<button type="submit" id="login-button">Register</button>
-		</form>
+				<input type="radio" name="gender" value="m" checked placeolder="Male">
+				<span style="color:white"> Male </span>
+				<input type="radio" name="gender" value="f">
+				<span style="color:white">Female </span><br>
+				<button type="submit" id="login-button">Register</button>
+			</form>
+		</div>
+
+		<ul class="bg-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
 	</div>
-	
-	<ul class="bg-bubbles">
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-</div>
 
 </body>
 
