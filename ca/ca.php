82ba86c1d4a53939a3fccb0d4de8d6f0d847cb73
Ashwani Yadav <ashyadavash@gmail.com>
2019-09-11 16:08:50 +0530

    updated logos and links

--- ca/ca.php
+++ ca/ca.php
@@ -5,12 +5,10 @@
     <title>Campus Ambassador</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
     <link rel="stylesheet" href="./css/main.css" />
     <link rel="stylesheet" href="./css/ca-styles.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-    <!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
 </head>
 
 <body>
@@ -21,15 +19,19 @@
         <h6 style="color:red">
             <!-- IIT Patna's -->
         </h6>
-        
-				<h1 style="color: #fff">Celesta'19</h1><br>
-				<h1 style="color: #fff">Campus Ambassador Programme</h1><br>
-				
+
+        <h1 style="color: #fff">Celesta'19</h1><br>
+        <h1 style="color: #fff">Campus Ambassador Programme</h1><br>
+
         <br><br><br><br><br>
         <center>
-            <div id="FB-Oauth">
-                <div class="fb-text"><h3><a href="../backend/user/ca_register.php">Sign-Up</a></h3></div>
-            </div>
+            <a href="../backend/user/ca_register.php">
+                <div id="FB-Oauth">
+                    <div class="fb-text">
+                        <h3>Sign-Up</h3>
+                    </div>
+                </div>
+            </a>
         </center>
         <div class="imgClass type1 img1"><img src="./images/img/1.png"></div>
         <div class="imgClass type1 img2"><img src="./images/img/2.png"></div>
@@ -43,7 +45,7 @@
         <header class="major container 95%">
             <h1 style="font-size: 40px">About us</h1><br>
             <p>
-                Celesta is the annual techno-management extravaganza celebrated with utmost pomp and vigour, in autumn. Since it's inception in 2010, it has grown into one of the most anticipated student organized youth festival. From NASA scientists to stupendous dancers, from ethical hackers to spellbinding singers, from game development to gaming war, from robotics to dramatics, from Model United Nations to foreign exchange conferences, from Sufi to Death Metal, Celesta, lives up to it's motto "Think. Dream. Live.", urging every individual to unleash their inner passion to reach their ultimate dream. Student fraternity who work day in day out, pouring out all of their heart into promoting intellectualism and creativity, while also maintaing the brand value of one of the most prestigious institute of the nation, IIT. Celesta, derived from the Sanskrit word meaning "quest" aims at unbridling the soul of every individual into the quest of finding one's self and reaching to his/her full potential. The latest edition of Celesta saw a footfall of around 15 K, of people belonging to a plethora of sectos such as computer geeks, gaming freaks, technoholics, music maniacs, dance enthusiasts and rock bands hailing from well-known institutes across the country. </p>
+                Celesta is the annual techno-management extravaganza celebrated with utmost pomp and vigour, in autumn. Since it's inception in 2010, it has grown into one of the most anticipated student organized youth festival. From NASA scientists to college geniuses, from ethical hackers to breathtaking treasure hunters, from game development to gaming war, from robotics to IPL Auction, from Model United Nations to foreign exchange conferences, Celesta, lives up to every expectations and more, urging every individual to unleash their inner passion to reach their ultimate dream. Student fraternity who work day in day out, pouring out all of their heart into promoting intellectualism and creativity, while also maintaing the brand value of one of the most prestigious institute of the nation, IIT. Celesta saw a footfall of around 4K, of people belonging to a plethora of sectos such as computer geeks, gaming freaks, technoholics, treasure hunters, and robotic geniuses hailing from well-known institutes across the country. </p>
         </header>
 
         <div class="box alt container">
@@ -52,7 +54,7 @@
                 <div class="content">
                     <h3>What is Campus Ambassador?</h3>
                     <p>
-                        The Campus Ambassador program is one of the leading publicity programs of Celesta The promotion of the fest in the respective colleges is assigned to the campus ambassadors. They serve as the intermediaries who bridge the gap between their college and our campus i.e, as a nodal point for any kind of communication or promotion. Campus ambassadors act as the face of one of the biggest cultural festival in the entire North-East India and are entrusted with the job of increasing the outreach of the fest through on field and social media publicity. The campus ambassadors are entitled to exciting prizes, apart from the coveted certificate and many other goodies.
+                        The Campus Ambassador program is one of the leading publicity programs of Celesta. The promotion of the fest in the respective colleges is assigned to the campus ambassadors. They serve as the intermediaries who bridge the gap between their college and our campus i.e, as a nodal point for any kind of communication or promotion. Campus ambassadors act as the face of one of the biggest techno-management festival in the entire North-East India and are entrusted with the job of increasing the outreach of the fest through on field and social media publicity. The campus ambassadors are entitled to exciting prizes, apart from the coveted certificate and many other goodies.
                     </p>
                 </div>
             </section>
@@ -73,9 +75,9 @@
                 <header>
                     <h3>CA's Experience</h3>
                 </header>
-                <blockquote> Celesta .. whenever I think about it...the word comes in my mind is....#teamwork & path provider of talents..Before celesta...I was just a boy in my college...but when I participated in 2k17 addition..i got a chance to work & perform with super talented guys.. now..it's just because of Anwesha..I am cultural Coordinator of my College..In short it brings ...a revolutionary changes in my life... Thanks team anwesha<br>-<b>Aryan S Prince</b> </blockquote>
-                <blockquote> Hello IITP, I remember I went Anwesha when I was in the first year of my college .I was scared but yeah that experience pulled me again and again and Anwesha 2K18 was my third year in a row.And this year was Damn amazing..The theme was magical ..The environment was spellbinding. And Yeah The thing you guys implemented this year this College ambassador thing was really great idea.. I really enjoyed working and sharing the notifications of Anwesha 2k18 In between of those times sometimes I started feeling like I am the part of the Team and yeah It was great being a part of such great environment and such enthusiastic People.. Anwesha is not just a college fest it feels like it's a Festival that comes every year and we all celebrate it together.. 🖤 <br>-<b>Mridul Choudhary</b> </blockquote>
-                <blockquote>As you mentioned Anwesha the biggest techno-cultural fest of north-east India being a campus ambassador for it was an amazing experience.You'll organised the whole function perfectly.I not only enjoyed being working as the campus ambassador but also enjoyed your fest.The participants from my college were also very happy by your organisation.Hope to continue to work with you all more as i gained a good experience. Thank you for selecting me as the campus ambassador of my college. <br>-<b>Kamal Kant</b> </blockquote>
+                <blockquote> Celesta .. whenever I think about it...the word comes in my mind is....#teamwork & path provider of talents..Before celesta...I was just a boy in my college...but when I participated in 2k17 addition..i got a chance to work & perform with super talented guys.. now..it's just because of Celesta..I am Tech Coordinator of my College..In short it brings ...a revolutionary changes in my life... Thanks team Celesta<br>-<b>Prince Singh</b> </blockquote>
+                <blockquote> Hello IITP, I remember I went to Celesta when I was in the first year of my college. I was scared but yeah that experience pulled me again and again and Celesta 2K18 was my third year in a row. And this year was damn amazing...The environment was spellbinding. And yeah, I really enjoyed working and sharing the notifications of Celesta 2k18. In between of those times sometimes I started feeling like I am the part of the Team and yeah, it was great being a part of such great environment and such enthusiastic people.. Celesta is not just a college fest, it feels like it's a Festival that comes every year and we all celebrate it together.. 🖤 <br>-<b>Mridul Choudhary</b> </blockquote>
+                <blockquote>As you mentioned, Celesta is the biggest techno-management fest of north-east India. Being a campus ambassador for it was an amazing experience. I not only enjoyed being working as the campus ambassador, but also enjoyed your fest. The participants from my college were also very happy by your organisation. Hope to continue to work with you all more as I gained a good experience. Thank you for selecting me as the campus ambassador of my college. <br>-<b>Kamal Kant</b> </blockquote>
             </section>
 
         </div>
