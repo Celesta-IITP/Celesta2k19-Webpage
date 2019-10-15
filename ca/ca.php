7a8edc90b7e0139cb54c8f49c5b9edadd00a8209
Shubham Kumar <shubham_1901mm32@iitp.ac.in>
2019-10-15 17:18:38 +0530

    feat: Change background of ca page

--- ca/ca.php
+++ ca/ca.php
@@ -18,11 +18,11 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 </head>
 
-<body>
+<body style="background-color:#181818;">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-#181818 bg-#181818">
         <!-- <div class="container"> -->
-  `      <a class="navbar-brand" href="../">CELESTA'19</a>
+  `      <a style="color:white;"class="navbar-brand" href="../"> C E L E S T A'19</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -30,25 +30,25 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="../">Home</a>
+                    <a style="color:LavenderBlush;" class="nav-link" href="../">Home</a>
                 </li>
                 <?php if($loggedIn) {?>
                     <li class="nav-item active">
-                        <a class="nav-link" href="../backend/user/profile.php">Profile</a>
+                        <a style="color:LavenderBlush;" class="nav-link" href="../backend/user/profile.php">Profile</a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="../backend/user/ca_logout.php">Logout</a>
+                        <a style="color:LavenderBlush;"class="nav-link" href="../backend/user/ca_logout.php">Logout</a>
                     </li>
                 <?php } else { ?>
                     <li class="nav-item active">
-                        <a class="nav-link" href="../backend/user/ca_register.php">Register</a>
+                        <a style="color:LavenderBlush;"class="nav-link" href="../backend/user/ca_register.php">Register</a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="../backend/user/reg.php">Login</a>
+                        <a style="color:LavenderBlush;"class="nav-link" href="../backend/user/reg.php">Login</a>
                     </li>
                 <?php } ?>
                 <li class="nav-item active">
-                    <a class="nav-link" href="https://internshala.com/internship/detail/campus-ambassador-programme-at-celesta-iit-patna1570467717">Register via Internshala</a>
+                    <a style="color:LavenderBlush;"class="nav-link" href="https://internshala.com/internship/detail/campus-ambassador-programme-at-celesta-iit-patna1570467717">Register via Internshala</a>
                 </li>
             </ul>
         </div>
@@ -100,11 +100,11 @@
     </div>
 
     <!-- Main -->
-    <div id="main">
+    <div id="main" style="background-color:#181818;">
 
         <header class="major container 95%">
             <h1 style="font-size: 40px">About us</h1><br>
-            <p>
+            <p style="color:#282828">
                 Celesta is the annual techno-management extravaganza celebrated with utmost pomp and vigour, in autumn. Since it's inception in 2010, it has grown into one of the most anticipated student organized youth festival. From NASA scientists to college geniuses, from ethical hackers to breathtaking treasure hunters, from game development to gaming war, from robotics to IPL Auction, from Model United Nations to foreign exchange conferences, Celesta, lives up to every expectations and more, urging every individual to unleash their inner passion to reach their ultimate dream. Student fraternity who work day in day out, pouring out all of their heart into promoting intellectualism and creativity, while also maintaing the brand value of one of the most prestigious institute of the nation, IIT. Celesta saw a footfall of around 4K, of people belonging to a plethora of sectos such as computer geeks, gaming freaks, technoholics, treasure hunters, and robotic geniuses hailing from well-known institutes across the country. </p>
         </header>
 
@@ -112,8 +112,8 @@
             <section class="feature left">
                 <a class="image icon  fa-retweet"><img src="./images/pic07.jpg" alt="" height="700" /></a>
                 <div class="content">
-                    <h3>What is Campus Ambassador?</h3>
-                    <p>
+                    <h3 style="color:white;">What is Campus Ambassador?</h3>
+                    <p style="color:LavenderBlush;">
                         The Campus Ambassador program is one of the leading publicity programs of Celesta. The promotion of the fest in the respective colleges is assigned to the campus ambassadors. They serve as the intermediaries who bridge the gap between their college and our campus i.e, as a nodal point for any kind of communication or promotion. Campus ambassadors act as the face of one of the biggest techno-management festival in the entire North-East India and are entrusted with the job of increasing the outreach of the fest through on field and social media publicity. The campus ambassadors are entitled to exciting prizes, apart from the coveted certificate and many other goodies.
                     </p>
                 </div>
@@ -121,8 +121,8 @@
             <section class="feature right">
                 <a class="image icon fa-sitemap"><img src="./images/pic07.jpg" alt="" height="700" /></a>
                 <div class="content">
-                    <h3>Why CA?</h3>
-                    <p>
+                    <h3 style=" color:white">Why CA?</h3>
+                    <p style="color:LavenderBlush;">
                         This is an extremely edifying opportunity where one gets to improve their oratory skills and managerial skills by interacting with people and encouraging them to take part in a fest of such huge grandeur. Apart from being advantageous on both personal and managerial grounds, working as a CA also comes with the additional perks of being conferred with an assured certificate from Celesta, IIT Patna validating your work as a campus ambassador. Not to forget are the exhilaratinging prizes and goodies that a CA receives.
                     </p>
                 </div>
@@ -135,9 +135,9 @@
                 <header>
                     <h3>CA's Experience</h3>
                 </header>
-                <blockquote> Celesta .. whenever I think about it...the word comes in my mind is....#teamwork & path provider of talents..Before celesta...I was just a boy in my college...but when I participated in 2k17 addition..i got a chance to work & perform with super talented guys.. now..it's just because of Celesta..I am Tech Coordinator of my College..In short it brings ...a revolutionary changes in my life... Thanks team Celesta<br>-<b>Prince Singh</b> </blockquote>
-                <blockquote> Hello IITP, I remember I went to Celesta when I was in the first year of my college. I was scared but yeah that experience pulled me again and again and Celesta 2K18 was my third year in a row. And this year was damn amazing...The environment was spellbinding. And yeah, I really enjoyed working and sharing the notifications of Celesta 2k18. In between of those times sometimes I started feeling like I am the part of the Team and yeah, it was great being a part of such great environment and such enthusiastic people.. Celesta is not just a college fest, it feels like it's a Festival that comes every year and we all celebrate it together.. 🖤 <br>-<b>Mridul Choudhary</b> </blockquote>
-                <blockquote>As you mentioned, Celesta is the biggest techno-management fest of north-east India. Being a campus ambassador for it was an amazing experience. I not only enjoyed being working as the campus ambassador, but also enjoyed your fest. The participants from my college were also very happy by your organisation. Hope to continue to work with you all more as I gained a good experience. Thank you for selecting me as the campus ambassador of my college. <br>-<b>Kamal Kant</b> </blockquote>
+                <blockquote style="color:#282828"> Celesta .. whenever I think about it...the word comes in my mind is....#teamwork & path provider of talents..Before celesta...I was just a boy in my college...but when I participated in 2k17 addition..i got a chance to work & perform with super talented guys.. now..it's just because of Celesta..I am Tech Coordinator of my College..In short it brings ...a revolutionary changes in my life... Thanks team Celesta<br>-<b>Prince Singh</b> </blockquote>
+                <blockquote style="color:#282828"> Hello IITP, I remember I went to Celesta when I was in the first year of my college. I was scared but yeah that experience pulled me again and again and Celesta 2K18 was my third year in a row. And this year was damn amazing...The environment was spellbinding. And yeah, I really enjoyed working and sharing the notifications of Celesta 2k18. In between of those times sometimes I started feeling like I am the part of the Team and yeah, it was great being a part of such great environment and such enthusiastic people.. Celesta is not just a college fest, it feels like it's a Festival that comes every year and we all celebrate it together.. 🖤 <br>-<b>Mridul Choudhary</b> </blockquote>
+                <blockquote style="color:#282828">As you mentioned, Celesta is the biggest techno-management fest of north-east India. Being a campus ambassador for it was an amazing experience. I not only enjoyed being working as the campus ambassador, but also enjoyed your fest. The participants from my college were also very happy by your organisation. Hope to continue to work with you all more as I gained a good experience. Thank you for selecting me as the campus ambassador of my college. <br>-<b>Kamal Kant</b> </blockquote>
             </section>
 
         </div>
