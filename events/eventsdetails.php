50b69cba7f7a122f751067140fcf7fb9356cab2b
Ashwani Yadav <ashyadavash@gmail.com>
2019-10-25 22:32:55 +0530

    added reload after succes event register

--- events/eventsdetails.php
+++ events/eventsdetails.php
@@ -288,19 +288,6 @@
               `;
         });
       }
-      else if (res.status === 302) {
-        res.message.forEach(mes => {
-          htmlData += `
-              <div class="toast fade show" style="z-index: 999">
-                  <div class="toast-header bg-warning">
-                      <strong class="mr-auto"><i class="fa fa-globe"></i> Warning</strong>
-                      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">&times;</button>
-                  </div>
-                  <div class="toast-body">${mes}</div>
-              </div>
-              `;
-        });
-      }
       else if (res.status === 404) {
         res.message.forEach(mes => {
           htmlData += `
@@ -327,19 +314,6 @@
               `;
         });
       }
-      else if (res.status === 302) {
-        res.message.forEach(mes => {
-          htmlData += `
-              <div class="toast fade show" style="z-index: 999">
-                  <div class="toast-header bg-warning">
-                      <strong class="mr-auto"><i class="fa fa-globe"></i> Error</strong>
-                      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">&times;</button>
-                  </div>
-                  <div class="toast-body">${mes}</div>
-              </div>
-              `;
-        });
-      }
       else if (res.status === 401) {
         res.message.forEach(mes => {
           htmlData += `
@@ -354,6 +328,7 @@
         });
       }
       else if (res.status === 202) {
+        location.reload();
         res.message.forEach(mes => {
           htmlData += `
               <div class="toast fade show" style="z-index: 999">
