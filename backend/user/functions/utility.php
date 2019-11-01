56051691deb396d3c2d1e2b9f64804b0ef8cba5a
Amartya Mondal <hayyoulistentome@gmail.com>
2019-11-01 21:30:30 +0530

    feat: Change email

--- backend/user/functions/utility.php
+++ backend/user/functions/utility.php
@@ -12,8 +12,8 @@ function send_email($email,$subject,$msg,$headers){
 	$mail->isSMTP();                                      // Set mailer to use SMTP
 	$mail->Host = 'tls://smtp.gmail.com';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = "dscappsocietyiitp@gmail.com";                 // SMTP username
-	$mail->Password = "appified@1504";  
+	$mail->Username = "celesta2019iitpatna@gmail.com";                 // SMTP username
+	$mail->Password = "celesta19@iitp.ac.in";
 	// $mail->Username = "celesta2k19@gmail.com";                 // SMTP username
 	// $mail->Password = "celesta19@iitp.ac.in"; 
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
